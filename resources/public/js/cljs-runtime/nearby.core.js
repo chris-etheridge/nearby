fd36d15a540a23c5683ef4e8236cbe6a9cbb7f9e
Chris Etheridge <chris.etheridge@cognician.com>
2018-02-19 21:19:06 +0200

    Update gignore

--- resources/public/js/cljs-runtime/nearby.core.js
+++
@@ -1,7 +0,0 @@
-goog.provide('nearby.core');
-goog.require('cljs.core');
-nearby.core.reload_BANG_ = (function nearby$core$reload_BANG_(){
-return cljs.core.prn.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2(["reloading"], 0));
-});
-
-//# sourceMappingURL=nearby.core.js.map
