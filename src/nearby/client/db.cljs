4bcd83225b2109ddcea5517baee6436434f4bbb9
Chris Etheridge <chris.etheridge@cognician.com>
2018-02-21 21:56:45 +0200

    Add client files

---
+++ src/nearby/client/db.cljs
@@ -0,0 +1,25 @@
+(ns nearby.client.db
+  (:require
+   [datascript.core :as d]))
+
+(def schema
+  {:messages {:db/cardinality :db.cardinality/many}})
+
+(def conn
+  (d/create-conn schema))
+
+(def *db (atom nil))
+
+(defn setup-db! []
+  (d/transact! conn
+               [{:db/id           -1
+                 :message/content "The first message!"}
+                {:db/id           -2
+                 :message/content "And then this message came along.."}]))
+
+(defn transact! [tx]
+  (d/transact! conn tx))
+
+(d/listen! conn
+           (fn [report]
+             (reset! *db (:db-after report))))
---
+++ src/nearby/client/ws.cljs
@@ -0,0 +1,35 @@
+(ns nearby.client.ws
+  (:require
+   [nearby.client.db :as db]
+   [clojure.string :as string]))
+
+;; TODO: actual on-message
+(defn on-message [val]
+  (db/transact! [{:db/id           -1
+                  :message/content val}]))
+
+(defn handle-error [e]
+  (prn "[ws] error! " e))
+
+(defn on-close [o]
+  (prn "[ws] socket closed" o))
+
+;; TODO: config var
+(def ws-uri
+  "ws://localhost:8080/ws")
+
+(def *socket (atom nil))
+
+(defn on-open [o]
+  (prn "[ws] socket opened" o)
+  (.send @*socket "hello")
+  o)
+
+(defn start! []
+  (let [sock (js/WebSocket. ws-uri)]
+    (prn :sock sock)
+    (set! (.-onmessage sock) on-message)
+    (set! (.-onopen sock) on-open)
+    (set! (.-onerror sock) handle-error)
+    (set! (.-onclose sock) on-close)
+    (reset! *socket sock)))
