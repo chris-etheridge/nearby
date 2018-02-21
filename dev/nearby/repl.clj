1873c48817bb01304f557398325aa88134c57f06
Chris Etheridge <chris.etheridge@cognician.com>
2018-02-21 21:57:17 +0200

    Wrap refactor nrepl

--- dev/nearby/repl.clj
+++ dev/nearby/repl.clj
@@ -3,11 +3,13 @@
    [nearby.server :as nearby.server]
    [cider.nrepl :as cider]
    [clojure.tools.nrepl.server :as nrepl.server]
-   [clojure.tools.namespace.repl :as namespace.repl]))
+   [clojure.tools.namespace.repl :as namespace.repl]
+   [refactor-nrepl.middleware :as refactor]))
 
 (defn -main [& args]
   (nrepl.server/start-server :port 6661
-                             :handler cider/cider-nrepl-handler)
+                             :handler (refactor/wrap-refactor
+                                       cider/cider-nrepl-handler))
   (spit ".nrepl-port" 6661)
   (println "Started repl on port 6661")
   (nearby.server/start!))
