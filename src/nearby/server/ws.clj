e14bab9ed22abed1ff194360b32fc87c0d8ec9bd
Chris Etheridge <chris.etheridge@cognician.com>
2018-02-24 16:54:55 +0200

    Snapshot and others

--- src/nearby/server/ws.clj
+++ src/nearby/server/ws.clj
@@ -2,46 +2,74 @@
   (:require
    [clojure.string :as str]
    [nearby.haversine :as haversine]
-   [immutant.web.async :as web.async]))
+   [immutant.web.async :as web.async]
+   [clojure.tools.logging :as logging]))
 
 (defonce *state (atom {:clients {}}))
 
 (def select-values
   (comp vec vals select-keys))
 
+(defn write-socket-data [data]
+  ;; TODO: transit
+  (pr-str data))
+
+(defn calculate-client-distance [{:client/keys [longitude latitude]} client]
+  (logging/info :e client :lng longitude :lat latitude)
+  (haversine/in-kilometers [longitude latitude]
+                           (select-values client [:client/longitude :client/latitude])))
+
+(defn connected-clients-txes [clients requesting-client]
+  (when-some [clients (seq clients)]
+    (for [{:client/keys [uuid longitude latitude] :as client} clients]
+      {:client/uuid      uuid
+       :client/distance  (calculate-client-distance client requesting-client)
+       :client/latitude  latitude
+       :client/longitude longitude})))
+
+(defn snapshot [*state requesting-client]
+  {:event/action :sync-clients
+   :event/uuid   (java.util.UUID/randomUUID)
+   :sync/time    (.getTime (java.util.Date.))
+   :db/clients   (connected-clients-txes (vals (:clients *state)) requesting-client)})
+
 (defn maybe-broadcast! [clients message]
-  (when (<= 1 (count clients))
-    (doseq [[_ {:ws/keys [channel client-uuid longitude latitude]}] clients
-            :let  [distance (->> (select-values message [:client/longitude :client/latitude])
-                                 (haversine/in-kilometers [longitude latitude]))]
+  (when (<= 2 (count clients))
+    (doseq [[_ {:client/keys [channel client-uuid longitude latitude]}] clients
+            :let  [distance (calculate-client-distance {:client/longitude longitude
+                                                        :client/latitude  latitude}
+                                                       message)]
             :when (>= 50 distance)] ;; TODO: config var
       (web.async/send! channel (pr-str ;; TODO: transit encoding
                                 (merge message {:client/uuid     (str client-uuid)
                                                 :client/distance distance}))))))
 
 (defn new-client [channel uuid coords]
-  (let [[lat lng] (str/split coords #",")]
-    {:ws/channel     channel
-     :ws/client-uuid uuid
-     :ws/raw-coords  coords
-     :ws/latitude    (Double/parseDouble lat)
-     :ws/longitude   (Double/parseDouble lng)}))
+  (logging/info "new-client" coords uuid)
+  (let [[lng lat] (str/split coords #",")]
+    (logging/info :lat lat :lng lng :split)
+    {:client/channel     channel
+     :client/client-uuid uuid
+     :client/raw-coords  coords
+     :client/latitude    (Double/parseDouble lat)
+     :client/longitude   (Double/parseDouble lng)}))
 
-(defn client-join-msg [{:ws/keys [latitude longitude client-uuid]}]
+(defn client-join-msg [{:client/keys [latitude longitude client-uuid]}]
   {:event/action     :client-join
+   :event/uuid       (java.util.UUID/randomUUID)
    :client/uuid      client-uuid
    :client/latitude  latitude
    :client/longitude longitude})
 
 (defn on-open-impl [coords channel]
-  (println "OPEN" channel coords)
-  (let [uuid   (java.util.UUID/randomUUID)
-        client (new-client channel uuid coords)]
-    (swap! *state assoc-in [:clients uuid] client)
-    (maybe-broadcast! (:clients @*state) (client-join-msg client))))
+  (let [uuid    (java.util.UUID/randomUUID)
+        nclient (new-client channel uuid coords)]
+    (println "OPEN" nclient)
+    (swap! *state assoc-in [:clients uuid] nclient)
+    (web.async/send! channel (write-socket-data (snapshot *state nclient)))
+    (maybe-broadcast! (:clients @*state) (client-join-msg nclient))))
 
 (defn prepare-handlers [{{coords "coords"} :params :as request}]
-  (prn :prepare coords)
   {:on-open (partial on-open-impl coords)
 
    :on-close (fn [channel {:keys [code reason]}]
