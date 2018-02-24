f69cc9de63ee429e1b31afbb5d42e2e37bd5f3d8
Chris Etheridge <chris.etheridge@cognician.com>
2018-02-24 16:55:03 +0200

    ES confirmed events

--- src/nearby/client/event_source.cljs
+++ src/nearby/client/event_source.cljs
@@ -9,34 +9,42 @@
   (.-EMPTY PersistentQueue))
 
 (defn new-state [db loop]
-  {:db     db
-   :loop   loop
-   :start  (js/Date.)
-   :events (new-queue)})
+  {:es/db     db
+   :es/loop   loop
+   :es/start  (js/Date.)
+   :es/events (new-queue)
+   :es/confirmed-events []})
 
 (defn dispatch! [event]
-  (swap! *state update :events conj event))
+  (swap! *state update :es/events conj event))
 
 (defmulti process!
   (fn [event]
     (:event/action event)))
 
-(defn new-client [uuid]
-  {:db/id       -1
-   :client/uuid uuid
-   :client/song ""})
+(defn new-client [client-data]
+  (merge {:db/id       -1
+          :client/song ""}
+         (select-keys client-data
+                      [:client/uuid :client/distance
+                       :client/longitude :client/latitude])))
 
 (defmethod process! :client-join [event]
-  [(new-client (:client/uuid event))])
+  [(new-client event)])
+
+(defmethod process! :sync-clients [event]
+  (prn :event event)
+  [])
 
 (defn new-loop! [db]
   (js/window.setInterval
    (fn [_]
-     (when-let [event (peek (:events @*state))]
+     (when-let [event (peek (:es/events @*state))]
        (prn :events/recv event)
        (let [tx (process! event)]
-         (swap! *state assoc :db (:db-after (d/with (:db @*state) tx)))
-         (swap! *state update :events pop))))
+         (swap! *state assoc :es/db (:db-after (d/with (:es/db @*state) tx)))
+         (swap! *state update :es/events pop)
+         (swap! *state update :es/confirmed-events conj event))))
    600))
 
 (defn start! [conn]
--- src/nearby/core.cljs
+++ src/nearby/core.cljs
@@ -11,29 +11,28 @@
 
 (rum/defc message-component < rum/reactive
   [*loop]
-  (let [db (:db (rum/react *loop))]
-    (prn :db db)
+  (let [{:es/keys [confirmed-events db]} (rum/react *loop)]
+    (prn :e confirmed-events :d db)
     [:.container
      [:.panel
       [:h1.panel__title "Messages"]
       [:.panel__content
-       (for [[id msg] (d/q '[:find ?id ?msg
-                             :in $
-                             :where
-                             [?id :message/content ?msg]]
-                           db)]
-         [:.message-content {:key id}
-          [:p (pr-str msg)]])]]
+       (for [event confirmed-events]
+         [:.message-content {:key (str "event/" (hash (:event/uuid event)))}
+          [:p (pr-str event)]])]]
      [:.panel.panel--muted
       [:h1.panel__title "Clients"]
-      [:.panel__content
+      [:.row.animation.fade-in.panel__content
        (for [[e v] (map #(vector (:e %) (:v %))
-                        (d/datoms db :avet :client/uuid))]
-         [:.client
-          [:.client__id
-           [:p e]]
-          [:.client__uuid
-           [:p v]]])]]]))
+                        (d/datoms db :avet :client/uuid))
+             :let [{:client/keys [uuid distance longitude latitude]} (d/touch (d/entity db e))]]
+         [:.client {:key (str "client/" v)}
+          [:.col-5.client__uuid
+           [:p uuid]]
+          [:.col-1.client__distance
+           [:p (str distance " km")]]
+          [:.col-4
+           [:p (str "lng/lat:" longitude "," latitude)]]])]]]))
 
 (defn mount-it! [*loop]
   (->> (js/document.getElementById "app")
